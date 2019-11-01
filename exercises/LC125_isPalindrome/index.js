9e2844eab7c1fb08a6f4fe193cc7ffb20d40129c
Chad Cramer <chcr6493@colorado.edu>
2019-11-01 10:41:24 -0700

    leetcode practice problems

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,20 @@
-function isPalindrome(s) {}
+function isPalindrome(s) {
+  // Sanitize input string
+  s = s.toLowerCase().replace(/[\W_]/g, '');
+  // Create left and right pointers
+  let left = 0;
+  let right = s.length - 1;
+  // while left is less than right
+  while (left < right) {
+    // if left is not equal to right, return false
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  // return true
+  return true;
+}
 
 module.exports = isPalindrome;
